3aa795a6bc8b3fc5f2d61c1e2fc7d84a9b0d11bf
Tanmay Varade <tvarade@vibrenthealth.com>
2024-06-07 22:23:37 +0530

    feat(atlantis): Setup CI/CD for atlantis

--- cluster/provider.tf
+++ cluster/provider.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   backend "s3" {
-#     bucket                  = "open-innovation-ai"
-#     key                     = "network.tfstate"
-#     region                  = "us-east-1"
-#     encrypt                 = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket                  = "terraform-test-eks"
+    key                     = "cluster/cluster.tfstate"
+    region                  = "ap-south-1"
+    encrypt                 = true
+  }
+}
 
 provider "aws" {
   region = var.aws_region
--- network/provider.tf
+++ network/provider.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   backend "s3" {
-#     bucket                  = "open-innovation-ai"
-#     key                     = "network.tfstate"
-#     region                  = "us-east-1"
-#     encrypt                 = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket                  = "terraform-test-eks"
+    key                     = "network/network.tfstate"
+    region                  = "ap-south-1"
+    encrypt                 = true
+  }
+}
 
 provider "aws" {
   region = var.aws_region
